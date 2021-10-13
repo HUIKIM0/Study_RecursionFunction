6c47571a4137cdaa1dd8609f07eb127e186e0ba8
HUIKIM0 <fehs123@naver.com>
2021-10-13 21:27:52 +0900

    update

--- Form1.cs
+++ Form1.cs
@@ -33,7 +33,7 @@ namespace Study_RecursionFunction
             int iLevel = (int)numLevel.Value;
 
             //컨트롤러 선택
-            enControlType eControlType = enControlType.UnKnown;
+            enControlType eControlType = enControlType.UnKnown;   
 
             if (rdoLabel.Checked) eControlType = enControlType.Label;
             else if (rdoTextBox.Checked) eControlType = enControlType.TextBox;
@@ -47,14 +47,17 @@ namespace Study_RecursionFunction
         }
 
         // ★ GroupBox찾기 -> Control찾기 -> 해당 Level의 Control 싹 다 text작업
-        private void ControlSearch(GroupBox CheckList, int iLevel, enControlType eControlType, string strTextBox)
+        private void ControlSearch(GroupBox gboxCheck, int iLevel, enControlType eControlType, string strTextBox)
         {
 
-            //  해당 GroupBox 안에 있는 Control을 하나 씩 가져와서 체크 foreach. Check List에 있는 컨트롤 검사 끝날때까지
-            foreach (var item in CheckList.Controls)
+            // 현재 GroupBox 안에 있는 Control을 하나 씩 가져오는 foreach. 
+            foreach (var item in gboxCheck.Controls)
             {
-                // Control 작업. 사용자 선택 Level과 일치하지 않으면 작업 X
-                if (CheckList.Text.Equals("Level " + iLevel))
+                // Control이 GroupBox일때 GroupBox.Text가 사용자가 선택한 Level 과 일치하지 않으면 작업 X
+                // ex) Level 1 선택 => gboxCheck.Text 가 Level 1 이여야 Level 1 Text 입력 작업. Level 2~3인 gboxCheck는 if문 패스
+
+                // ★처음 실행해준 경우 gboxCheck.Text가 Check List 이기 때문에 이 if 문 패스한다★
+                if (gboxCheck.Text.Equals("Level " + iLevel))
                 {
                     switch (eControlType)  //Label,TextBox, Button
                     {
@@ -62,21 +65,21 @@ namespace Study_RecursionFunction
                             if (item is Label)
                             {
                                 ((Label)item).Text = strTextBox;
-                                lboxResult.Items.Add(string.Format("현재 GroupBox : {0}, Label Text : {1} 로 변경", CheckList.Text, strTextBox));
+                                lboxResult.Items.Add(string.Format("현재 GroupBox : {0}, Label Text : {1} 로 변경", gboxCheck.Text, strTextBox));
                             }
                             break;
                         case enControlType.TextBox:
                             if (item is TextBox)
                             {
                                 ((TextBox)item).Text = strTextBox;
-                                lboxResult.Items.Add(string.Format("현재 GroupBox : {0}, TextBox Text : {1} 로 변경", CheckList.Text, strTextBox));
+                                lboxResult.Items.Add(string.Format("현재 GroupBox : {0}, TextBox Text : {1} 로 변경", gboxCheck.Text, strTextBox));
                             }
                             break;
                         case enControlType.Button:
                             if (item is Button)
                             {
                                 ((Button)item).Text = strTextBox;
-                                lboxResult.Items.Add(string.Format("현재 GroupBox : {0}, Button Text : {1} 로 변경", CheckList.Text, strTextBox));
+                                lboxResult.Items.Add(string.Format("현재 GroupBox : {0}, Button Text : {1} 로 변경", gboxCheck.Text, strTextBox));
                             }
                             break;
                         default:
@@ -84,19 +87,20 @@ namespace Study_RecursionFunction
                     }
                 }
 
-                // 처음 돌리면 여기먼저 온다 Check List 아래에 큰 Groupbox 3개니까
-                // GroupBox안에 GroupBox가 또 있는 경우 또 다른 GroupBox를 기준으로 조사 ex)Check List였던 기준을 Level 1으로
-                // 현재 돌고있는 함수 다시 호출.
+                // 사용자가 선택한 Level과는 안 맞지만  foreach로 꺼내온 컨트롤이 GroupBox라면
+                // 현재 돌고있는 함수 다시 호출. 재귀함수. ControlSearch함수 호출 시 다음 GroupBox를 새로운 기준으로 준다
                 if (item is GroupBox)
                 {
-                    lboxResult.Items.Add(string.Format("현재 GroupBox : {0}, 다음 GroupBox : {1} 로 이동", CheckList.Text, ((GroupBox)item).Text));
+                    lboxResult.Items.Add(string.Format("현재 GroupBox : {0}, 다음 GroupBox : {1} 로 이동", gboxCheck.Text, ((GroupBox)item).Text));
                     ControlSearch((GroupBox)item, iLevel, eControlType, strTextBox);
                 }
+
             }
 
-            if(gboxCheckList == CheckList)
+            if(gboxCheckList == gboxCheck)
             {
                 lboxResult.Items.Add(string.Format("END"));
+                lboxResult.Items.Add(string.Format("\n"));
             }
         }
     }
