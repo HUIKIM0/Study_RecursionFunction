41ebf14dbbde1919bc9b3e9505aba571b05e6ca7
HUIKIM0 <fehs123@naver.com>
2022-03-22 18:48:22 +0900

    update

--- Form1.cs
+++ Form1.cs
@@ -51,16 +51,15 @@ namespace Study_RecursionFunction
         private void ControlSearch(GroupBox gboxCheck, int iLevel, enControlType eControlType, string strTextBox)
         {
 
-            // 현재 GroupBox 안에 있는 Control을 하나 씩 가져오는 foreach. 
+            // 현재 GroupBox 안에 있는 Control을 하나 씩 가져오는 foreach.
             foreach (var item in gboxCheck.Controls)
             {
-                // Control이 GroupBox일때 GroupBox.Text가 사용자가 선택한 Level 과 일치하지 않으면 작업 X
-                // ex) Level 1 선택 => gboxCheck.Text 가 Level 1 이여야 Level 1 Text 입력 작업. Level 2~3인 gboxCheck는 if문 패스
 
                 // ★처음 실행해준 경우 gboxCheck.Text가 Check List 이기 때문에 이 if 문 패스한다★
+                // Level 그룹 찾기
                 if (gboxCheck.Text.Equals("Level " + iLevel))
                 {
-                    switch (eControlType)  //Label,TextBox, Button
+                    switch (eControlType)  //그룹 안의 Label,TextBox, Button 찾기
                     {
                         case enControlType.Label:
                             if (item is Label)
@@ -92,7 +91,8 @@ namespace Study_RecursionFunction
                 }
 
                 // foreach로 꺼내온 컨트롤이 Label,TextBox,Button이 아닌 ★GroupBox라면
-                // 재귀함수. ControlSearch함수 호출 시 다음 GroupBox를 새로운 기준으로 준다
+                // ex) GroupBox Level1의  Label 작업 완료 -> Level2로 가야함(새로운 GroupBox)
+                // !재귀함수! ControlSearch함수 호출 시 다음 GroupBox를 새로운 기준으로 준다
                 if (item is GroupBox)
                 {
 
@@ -110,6 +110,7 @@ namespace Study_RecursionFunction
                 lboxResult.Items.Add(string.Format("\n"));
             }
         }
+
 
     }
 }
